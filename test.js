cd9310efe267bc94f8028aaf9736e311fde6350c
Jeevakrishantest <jeevakrishan96@gmail.com>
2024-05-24 17:54:29 +0530

    test
    first

---
+++ test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 10},
+    { duration: '30s', target: 20 },
+    { duration: '5s', target: 2 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  
+  sleep(1);
+}
